22c5966c9d74584ccc40b4fa6ba1b187f25de4c3
KyleTryon <kyle.tryon@outlook.com>
2022-09-18 00:43:25 -0400

    feat: add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,24 @@
+import '../src/styles/globals.css';
+import '../src/stories/assets/storybook.css';
+
+import * as NextImage from 'next/image';
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  previewTabs: {
+    'storybook/docs/panel': { index: -1 },
+  },
+};
